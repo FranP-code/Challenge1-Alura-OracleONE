209f548c39f20f5101cfb9774327a64936e07f4d
Francisco Pessano <franpessano1@gmail.com>
2022-05-02 15:31:20 -0300

    Función del copy button modularizada

--- script/script.js
+++ script/script.js
@@ -99,27 +99,3 @@ decryptButton.addEventListener("click", () => {
         <p class="result">${decryptedText}</p>
     `
 })
-
-//copyButton event
-const copyButton = document.querySelector('aside.history button.copy-last-text-message')
-
-copyButton.addEventListener("click", () => {
-
-    const elements = historyTexts.querySelectorAll(".result")
-
-    if (elements.length === 0) {
-        return
-    }
-
-    navigator.clipboard.writeText(elements[elements.length - 1].textContent)
-
-    copyButton.classList.add("element-copied")
-    
-    const buttonTextContent = copyButton.textContent
-    copyButton.textContent = "👌"
-
-    setTimeout(() => {
-        copyButton.textContent = buttonTextContent 
-        copyButton.classList.remove("element-copied")
-    }, 1000)
-})
